1dc53a592a5426583edbf698d77f81d6e62e1b79
Vitaly Sinilin <vs@kp4.ru>
2016-07-30 18:57:13 +0300

    acpid: cosmetic changes

--- acpid/default.sh
+++ acpid/default.sh
@@ -5,21 +5,21 @@
 
 set $*
 
-case "$1" in
-    button/power)
-        case "$2" in
-            PWRF)
-                /sbin/init 0
-                ;;
-            *)
-                logger "ACPI action $2 is not defined"
-                ;;
-        esac
-        ;;
+case $1 in
+	button/power)
+		case $2 in
+			PWRF)
+				/sbin/init 0
+				;;
+			*)
+				logger "ACPI action $2 is not defined"
+				;;
+		esac
+		;;
 
-    *)
-        logger "ACPI group $1 / action $2 is not defined"
-        ;;
+	*)
+		logger "ACPI group $1 / action $2 is not defined"
+		;;
 esac
 
 # End of file
